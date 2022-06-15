a24edb9f896ccddfc4226ad3e3968975b5491f58
Walter Nuccio <walter.nuccio@agilelab.it>
2022-06-15 09:46:51 +0200

    remove default value for emailAddress

--- src/main/scala/greeting/Friend.scala
+++ src/main/scala/greeting/Friend.scala
@@ -1,6 +1,6 @@
 package walt.kata
 package greeting
 
-case class Friend(firstName: String, emailAddress: EmailAddress = EmailAddress("")) {
+case class Friend(firstName: String, emailAddress: EmailAddress) {
 
 }
