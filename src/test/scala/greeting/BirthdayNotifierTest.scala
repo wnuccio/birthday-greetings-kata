c78e0368e97d384d5ab1e908ee98cb58c04f9b45
Walter Nuccio <walter.nuccio@agilelab.it>
2022-06-15 09:45:01 +0200

    remove Greeting logic and related tests

--- src/test/scala/greeting/BirthdayNotifierTest.scala
+++ src/test/scala/greeting/BirthdayNotifierTest.scala
@@ -6,27 +6,16 @@ import org.scalatest.matchers.should
 
 /***
  * Test case list:
- * - one friend -> one greeting with verification of the email address
- * - one friend -> one greeting with verification of the message
  * - one friend -> no greeting if today is not her birthday
  * - two friend -> one greeting since it is the birthday of the first one
  *
  * Done
  * - one friend -> one greeeting
+ * - one friend -> one greeting with verification of the email address
+ * - one friend -> one greeting with verification of the message
  */
 
 class BirthdayNotifierTest extends AnyFlatSpec with should.Matchers {
-
-  "The BirthdayNotifier " should "send one greeting" in {
-    val friends: Seq[Friend] = Seq(Friend("John"))
-    val emailSender = new EmailSenderMock()
-    val birthdayNotifier = new BirthdayNotifier(friends, emailSender)
-
-    birthdayNotifier.sendGreetings()
-
-    emailSender.greetings.size shouldBe 1
-    emailSender.greetings.head shouldBe Greeting("John")
-  }
 
   "The BirthdayNotifier " should "send one greeting to the specified address" in {
     val friends: Seq[Friend] = Seq(Friend("John", EmailAddress("john.doe@foobar.com")))
--- src/test/scala/greeting/EmailSenderMock.scala
+++ src/test/scala/greeting/EmailSenderMock.scala
@@ -3,17 +3,10 @@ package greeting
 
 class EmailSenderMock() extends EmailSender {
 
-  var greetings: Seq[Greeting] = Seq.empty
-
   override def sendGreetingsTo(friends: Seq[Friend]): Unit = {
-    friends
-      .map(friend => Greeting(friend.firstName))
-      .foreach(greeting => greetings = greetings :+ greeting)
-
     friends
       .map(friend => Email(friend.firstName, friend.emailAddress))
       .foreach(email => emails = emails :+ email)
-
   }
 
   var emails: Seq[Email] = Seq.empty
