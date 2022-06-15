4ecd2d6d17f5bb495441542d1ca3018d314c1a71
Walter Nuccio <walter.nuccio@agilelab.it>
2022-06-15 09:21:43 +0200

    add test case list

--- src/test/scala/greeting/BirthdayNotifierTest.scala
+++ src/test/scala/greeting/BirthdayNotifierTest.scala
@@ -4,6 +4,16 @@ package greeting
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
+/***
+ * Test case list:
+ * - one friend -> one greeting with verification of the email address
+ * - one friend -> no greeting if today is not her birthday
+ * - two friend -> one greeting since it is the birthday of the first one
+ *
+ * Done
+ * - one friend -> one greeeting
+ */
+
 class BirthdayNotifierTest extends AnyFlatSpec with should.Matchers {
 
   "The BirthdayNotifier " should "send one greeting" in {
