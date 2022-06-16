223f5c5155c589e337001db41b8653c933629810
Walter Nuccio <walter.nuccio@agilelab.it>
2022-06-16 09:42:14 +0200

    separate configuration from invocation in main

--- src/main/scala/BirthdayGreetings.scala
+++ src/main/scala/BirthdayGreetings.scala
@@ -1,46 +1,11 @@
 package walt.kata
 
-import date.Date
-import email.{Email, EmailGateway, EmailSender}
-import friendsfile.FriendsFile
-import greeting.{BirthdayGreetingsFacade, Clock, FriendRepository, GreetingsSender}
-
-import java.io.{FileWriter, PrintWriter}
-import java.time.LocalDate
-import scala.io.Source
+import config.Config
 
 object BirthdayGreetings {
 
   def main(args: Array[String]): Unit = {
-    val friendRepository: FriendRepository = createFriendRepository(args)
-    val clockReader: Clock = createClock(args)
-    val greetingsSender: GreetingsSender = createGreetingsSender(args)
-
-    new BirthdayGreetingsFacade(friendRepository, greetingsSender, clockReader).sendGreetings()
-  }
-
-  private def createGreetingsSender(args: Array[String]) = {
-    val outputFile = args(2)
-    val emailSender = new EmailSender(new EmailGateway {
-      val writer = new PrintWriter(new FileWriter(s"src/main/resources/$outputFile"))
-
-      override def sendEmail(email: Email): Unit = {
-        writer.append(s"email to: ${email.personName}, ${email.address.value}\n")
-        writer.flush()
-      }
-    })
-    emailSender
-  }
-
-  private def createClock(args: Array[String]): Clock = {
-    val clockFile = args(1)
-    new Clock {
-      override def today: LocalDate = Date(Source.fromResource(clockFile).getLines().next())
-    }
-  }
-
-  private def createFriendRepository(args: Array[String]): FriendRepository = {
-    val inputFile = args(0)
-    new FriendsFile(inputFile)
+    val greetingsFacade = new Config(args).greetingsFacade
+    greetingsFacade.sendGreetings()
   }
 }
---
+++ src/main/scala/config/Config.scala
@@ -0,0 +1,42 @@
+package walt.kata
+package config
+
+import date.Date
+import email.{Email, EmailGateway, EmailSender}
+import friendsfile.FriendsFile
+import greeting.{BirthdayGreetingsFacade, Clock, FriendRepository}
+
+import java.io.{FileWriter, PrintWriter}
+import java.time.LocalDate
+import scala.io.Source
+
+class Config(val args: Array[String]) {
+
+  private def greetingsSender() = {
+    val outputFile = args(2)
+    val emailSender = new EmailSender(new EmailGateway {
+      val writer = new PrintWriter(new FileWriter(s"src/main/resources/$outputFile"))
+
+      override def sendEmail(email: Email): Unit = {
+        writer.append(s"email to: ${email.personName}, ${email.address.value}\n")
+        writer.flush()
+      }
+    })
+    emailSender
+  }
+
+  private def clock(): Clock = {
+    val clockFile = args(1)
+    new Clock {
+      override def today: LocalDate = Date(Source.fromResource(clockFile).getLines().next())
+    }
+  }
+
+  private def friendRepository(): FriendRepository = {
+    val inputFile = args(0)
+    new FriendsFile(inputFile)
+  }
+
+  val greetingsFacade = new BirthdayGreetingsFacade(friendRepository(), greetingsSender(), clock())
+
+}
