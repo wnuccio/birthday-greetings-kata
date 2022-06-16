bf42544cb68297f66bfeb69ba8b7d5b95dd1ca3d
Walter Nuccio <walter.nuccio@agilelab.it>
2022-06-16 12:38:42 +0200

    change config to use variables instead of methods

--- src/main/scala/it/walt/kata/config/Config.scala
+++ src/main/scala/it/walt/kata/config/Config.scala
@@ -6,22 +6,18 @@ import it.walt.kata.features.greetings.{BirthdayGreetingsFacade, FriendRepositor
 import it.walt.kata.infrastructure._
 
 class Config(val args: Array[String]) {
-  val greetingsFacade = new BirthdayGreetingsFacade(friendRepository(), clock(), greetingsSender())
+  lazy val greetingsFacade = new BirthdayGreetingsFacade(friendRepository, clock, greetingsSender)
 
-    private def friendRepository(): FriendRepository = {
-      val inputFile = if (args.length == 3) args(0) else "friends.txt"
-      new FriendsFile(inputFile)
-    }
+  private lazy val friendRepository: FriendRepository =
+    new FriendsFile(if (isAcceptanceTest) args(0) else "friends.txt")
 
-    private def clock(): Clock = {
-      if (args.length == 3) new ClockReadFromFile(args(1))
-      else new RealClock
-    }
+  private lazy val clock: Clock =
+    if (isAcceptanceTest) new ClockReadFromFile(args(1)) else new RealClock
 
-    private def greetingsSender() = {
-      val emailGateway = if (args.length == 3)
-        new WriteEmailOnFile(args(2)) else new WriteEmailOnConsole()
+  private lazy val greetingsSender = new EmailSender(emailGateway)
 
-      new EmailSender(emailGateway)
-    }
+  private lazy val emailGateway = if (isAcceptanceTest)
+      new WriteEmailOnFile(args(2)) else new WriteEmailOnConsole()
+
+  private lazy val isAcceptanceTest = args.length == 3
   }
