0d8ad721f33be410c6fbd06b392af1ebae877089
Walter Nuccio <walter.nuccio@agilelab.it>
2022-06-16 10:17:31 +0200

    refactor configuration class

--- src/main/scala/config/Config.scala
+++ src/main/scala/config/Config.scala
@@ -11,31 +11,44 @@ import java.time.LocalDate
 import scala.io.Source
 
 class Config(val args: Array[String]) {
-
   val greetingsFacade = new BirthdayGreetingsFacade(friendRepository(), clock(), greetingsSender())
 
   private def friendRepository(): FriendRepository = {
-    val inputFile = args(0)
+    val inputFile = if (args.length == 3) args(0) else "friends.txt"
     new FriendsFile(inputFile)
   }
 
   private def clock(): Clock = {
-    val clockFile = args(1)
-    new Clock {
-      override def today: LocalDate = Date(Source.fromResource(clockFile).getLines().next())
-    }
+    if (args.length == 3) new ReadTodayFromFile(args(1))
+    else new RealClock
   }
 
   private def greetingsSender() = {
-    val outputFile = args(2)
-    val emailSender = new EmailSender(new EmailGateway {
-      val writer = new PrintWriter(new FileWriter(s"src/main/resources/$outputFile"))
+    val emailGateway = if (args.length == 3)
+      new WriteEmailOnFile(args(2)) else new WriteEmailOnConsole()
 
-      override def sendEmail(email: Email): Unit = {
+    new EmailSender(emailGateway)
+  }
+
+  private class ReadTodayFromFile(clockFile: String) extends Clock {
+    override def today: LocalDate = Date(Source.fromResource(clockFile).getLines().next())
+  }
+
+  private class RealClock() extends Clock {
+    override def today: LocalDate = LocalDate.now()
+  }
+
+  private class WriteEmailOnFile(outputFile: String) extends EmailGateway {
+    val writer = new PrintWriter(new FileWriter(s"src/main/resources/$outputFile"))
+    override def sendEmail(email: Email): Unit = {
         writer.append(s"email to: ${email.personName}, ${email.address.value}\n")
         writer.flush()
       }
-    })
-    emailSender
-  }
+    }
+
+  private class WriteEmailOnConsole() extends EmailGateway {
+    override def sendEmail(email: Email): Unit = {
+      print(s"email to: ${email.personName}, ${email.address.value}\n")
+      }
+    }
 }
