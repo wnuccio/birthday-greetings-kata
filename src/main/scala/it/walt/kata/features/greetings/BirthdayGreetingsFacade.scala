0d876b818ecba990f3c95e864fd123c533efb830
Walter Nuccio <walter.nuccio@agilelab.it>
2022-06-17 11:22:20 +0200

    extract subsets of friends as variables

--- src/main/scala/it/walt/kata/features/greetings/BirthdayGreetingsFacade.scala
+++ src/main/scala/it/walt/kata/features/greetings/BirthdayGreetingsFacade.scala
@@ -5,25 +5,25 @@ import it.walt.kata.features.date.Clock
 class BirthdayGreetingsFacade(friendRepository: FriendRepository, clock: Clock, greetingsSender: GreetingsSender) {
 
   def sendGreetings(): Unit = {
-    friendRepository
-      .allFriends
-      .filter(friend => friend.isBirthdate(clock.today))
+    birthdayFriends
       .foreach(friend => greetingsSender.sendGreetingsTo(friend))
   }
 
   def sendRemainders(): Unit = {
-    val birthdays = friendRepository
-      .allFriends
-      .filter(friend => friend.isBirthdate(clock.today))
-
-    friendRepository
-      .allFriends
-      .filter(friend => ! birthdays.contains(friend))
+    otherFriends
       .foreach(friend => {
-        birthdays.foreach(birthdayFriend => {
+        birthdayFriends.foreach(birthdayFriend => {
           greetingsSender.sendRemainderTo(friend, birthdayFriend)
         })
       })
 
   }
+
+  private lazy val birthdayFriends: Seq[Friend] = friendRepository
+      .allFriends
+      .filter(friend => friend.isBirthdate(clock.today))
+
+  private lazy val otherFriends = friendRepository
+      .allFriends
+      .filter(friend => !birthdayFriends.contains(friend))
 }
